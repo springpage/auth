208d322874de6be81fe7fdf9c9b6e92cf8989465
Spring Page <spring616666@gmail.com>
2018-06-16 13:28:18 -0700

    1

---
+++ src/app.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: "AIzaSyBEe4XRMFbERetHm-y4pyxL1uH060mgvIQ",
+    authDomain: "auth-8e630.firebaseapp.com",
+    databaseURL: "https://auth-8e630.firebaseio.com",
+    projectId: "auth-8e630",
+    storageBucket: "auth-8e630.appspot.com",
+    messagingSenderId: "1028331235274"
+  });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
